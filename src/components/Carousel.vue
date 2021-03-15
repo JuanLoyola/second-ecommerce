37882f55b0f055ae75e338dbe599a05d87f95080
Loyola <juanloyola@hotmail.com>
2021-03-15 15:39:28 -0300

    carousel v1

---
+++ src/components/Carousel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="carousel">
+    <slot></slot>
+    <button @click="next" class="next">
+      <i class="fas fa-arrow-right"></i>
+    </button>
+    <button @click="prev" class="prev">
+      <i class="fas fa-arrow-left"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+
+  methods: {
+    next () {
+      this.$emit('next')
+    },
+    prev () {
+      this.$emit('prev')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  position: relative;
+  width: 600px;
+  height: 350px;
+  overflow: hidden;
+  margin: 30px 0;
+
+  left: 25%;
+
+  button{
+    position: absolute;
+    height: 40px;
+    width: 50px;
+    top: calc(50% - 20px);
+    background-color: rgba(0, 0, 0, 0.5);
+    border: none;
+    color: #fff;
+  }
+
+  button:focus,
+  button:hover {
+    outline: none;
+    cursor: pointer;
+  }
+
+  .next {
+    right: 0;
+  }
+
+  .prev {
+    left: 0;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,73 @@
   <div class="container">
     <div class="home">
     <TheOne/>
+    <Carousel
+    @next="next"
+    @prev="prev"
+    >
+      <Carousel-slide
+        v-for="(slide, index) in slides"
+        :key="slide"
+        :index="index"
+        :visibleSlide = "visibleSlide"
+        :direction = "direction"
+        >
+        <img class="img" :src="slide">
+      </Carousel-slide>
+    </Carousel>
   </div>
   </div>
 </template>
 
 <script>
 import TheOne from '@/components/banners/TheOne.vue'
+import Carousel from '../components/Carousel.vue'
+import CarouselSlide from '@/components/CarouselSlide.vue'
 
 export default {
   name: 'Home',
+
   components: {
-    TheOne
+    TheOne,
+    Carousel,
+    CarouselSlide
+  },
+
+  data () {
+    return {
+      slides: [
+        'https://images.unsplash.com/photo-1584305574647-0cc949a2bb9f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1605036927639-1555563331a9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1535585209827-a15fcdbc4c2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+      ],
+      visibleSlide: 0,
+      direction: 'left'
+    }
+  },
+
+  computed: {
+    slidesLen () {
+      return this.slides.length
+    }
+  },
+
+  methods: {
+    next () {
+      if (this.visibleSlide >= this.slidesLen - 1) {
+        this.visibleSlide = 0
+      } else {
+        this.visibleSlide++
+      }
+      this.direction = 'left'
+    },
+    prev () {
+      if (this.visibleSlide <= 0) {
+        this.visibleSlide = this.slidesLen - 1
+      } else {
+        this.visibleSlide--
+      }
+      this.direction = 'right'
+    }
   }
 }
 </script>
@@ -26,6 +82,11 @@ export default {
 
   .home {
     width: 65%;
+
+    .img {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 
